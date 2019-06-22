12bfa89bf9c2b1102537b1015f41be7d8dfd6d05
Caleb Woods <woodscaleb@gmail.com>
2019-06-22 15:12:24 -0700

    added profiles

---
+++ db/migrate/20190622220908_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :address
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
